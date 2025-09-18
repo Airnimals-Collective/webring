3b82dc64ef7e46d6012a949abfed5f7419490e9d
Airnimals-Collective <directories@airnimals.org>
2025-09-18 14:18:28 +0200

    added config area to airnimals.js

--- airnimals.js
+++ airnimals.js
@@ -1,10 +1,24 @@
-  const img = document.getElementById('nextLink');
+// ### CONFIG AREA ###
+
+const serURL = 'https://domain.com' ;
+// put the url here where the script is hosted
+
+const ringNAME = 'Airnimals Collective' ;
+// specify the name of your webring
+
+const useANIMATION = 'true' ;
+// specify whether or not the title link is animated (insert true or false"
+
+
+
+
+const img = document.getElementById('nextLink');
   const link = document.getElementById('prevLink');
 
   link.addEventListener('mouseover', () => {
-    img.classList.add('blink');
+    img.classList.add('AIRNIMALSblink');
   });
 
   link.addEventListener('mouseout', () => {
-    img.classList.remove('blink');
+    img.classList.remove('AIRNIMALSblink');
   });
