d52853a18cbf654aed1b6a4bcb62b6f7d0939d9c
Airnimals-Collective <directories@airnimals.org>
2025-09-28 12:08:02 +0200

    Update airnimals.js

--- airnimals.js
+++ airnimals.js
@@ -1,15 +1,23 @@
 // ### CONFIG AREA ###
 
 const serURL = 'https://domain.com' ;
-// put the url here where the script is hosted e.g. https://airnimals.org
+// put the url here that the script is hosted on e.g. https://airnimals.org
 
 const ringNAME = 'Airnimals Collective' ;
-// specify the name of your webring
+// specify the name of your webring that will be displayed above the links
+
+const ringID = 'AIR' ;
+// put in the ring id that was given to you by the webring host (you can choose your own id when self-hosting the script)
 
 const useANIMATION = 'true' ;
-// specify whether or not the title link is animated (insert true or false"
+// specify whether or not the title link should be animated (insert true or false)
 
+const randomLINK = 'true' ;
+// specify whether or not a link to a random webring member should be displayed (insert true or false)
 
+const nextprevLINK = 'true' ;
+// specify whether or not links to the previous and next webring member should be displayed (insert true or false)
+// IMPORTANT you whether have to set randomLINK or nextprevLINK to true or set both to true, otherwise no links will show up
 
 
 const img = document.getElementById('nextLink');
